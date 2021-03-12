0ca5e7358ac76ffd293c54a1c14f7962ce847da0
drewmeltpool <drewmelt132@gmail.com>
2021-03-12 14:14:49 +0300

    git pull

--- gulpfile.js
+++ gulpfile.js
@@ -55,8 +55,6 @@ const build = gulp.series(
     buildJS,
     watcher
 )
-var ghPages = require('gulp-gh-pages');
-gulp.task('deploy', function() { return gulp.src('./build/**/*').pipe(ghPages()); });
 
 gulp.task("clean", cleanBuild)
 gulp.task("buildCSS", buildCSS)
