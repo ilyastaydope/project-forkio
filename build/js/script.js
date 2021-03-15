b7a6bd1d60986aacc8ccef3824450f9324d86c69
Ilya Stepenko <ilya.stepenko@gmail.com>
2021-03-15 18:43:57 +0200

    some fixes in scss, html

--- build/js/script.js
+++ build/js/script.js
@@ -1,8 +1,8 @@
-const burger = document.querySelector(".hamburger");
+const burger = document.querySelector(".btn--hamburger");
 const navList = document.querySelector('.nav__list')
 
 document.addEventListener('click', e => {
-    const fullBurger = e.target.closest(".hamburger");
+    const fullBurger = e.target.closest(".btn--hamburger");
     if (fullBurger) {
         navList.classList.toggle("visible");
         burger.classList.toggle("active");
