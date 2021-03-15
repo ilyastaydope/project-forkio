f8668a23ff5dd0c7154489c7bdd5cb49167c78e3
drewmeltpool <43030846+drewmeltpool@users.noreply.github.com>
2021-03-15 19:06:00 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
-let burger = document.querySelector(".hamburger");
-let navList = document.querySelector('.nav__list')
+const burger = document.querySelector(".hamburger");
+const navList = document.querySelector('.nav__list')
 
 document.addEventListener('click', e => {
-    let fullBurger = e.target.closest(".hamburger");
+    const fullBurger = e.target.closest(".hamburger");
     if (fullBurger) {
         navList.classList.toggle("visible");
         burger.classList.toggle("active");
     }
+    navList.classList.remove("visible");
+    burger.classList.remove("active");
 })
