167ea9ddcff5ba1b2755e99395be8b2f326a62ed
Ilya Stepenko <ilya.stepenko@gmail.com>
2021-03-15 17:50:00 +0200

    add script and burger menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 let burger = document.querySelector(".hamburger");
 let navList = document.querySelector('.nav__list')
 
-document.addEventListener('click', e =>{
-    if(e.target === burger){
+document.addEventListener('click', e => {
+    let fullBurger = e.target.closest(".hamburger");
+    if (fullBurger) {
         navList.classList.toggle("visible");
+        burger.classList.toggle("active");
     }
 })
